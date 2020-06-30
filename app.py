052959e361a45fa8c9ac656abb3c2e6cb893f3ef
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-06-30 12:23:08 +0100

    added requirements.txt, base code for flask, and base html

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+import os
+from os import path
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = '1up_loot'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
