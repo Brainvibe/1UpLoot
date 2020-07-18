d3b7503edd52e794dc961834c28463869f2ab38c
Nuno Nunes <nuno.t.nunes@gmail.com>
2020-07-18 13:14:33 +0100

    login system now fully working

--- app.py
+++ app.py
@@ -18,41 +18,47 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     if 'username' in session:
-        return render_template('index.html')
+        return 'Logged in as ' + session['username']
+        
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
     return render_template("index.html")
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-        
-    if request.method == 'POST':
-        login_user = users.find_one({'name' : request.form['username']})
-        print(login_user)
-        if login_user:
-            if bcrypt.hashpw(request.form['pass'], login_user['password'] == login_user['password']):
-               session['username'] = request.form['username']
+    login_user = users.find_one({'name' : request.form['username']})
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
             return redirect(url_for('index'))
-            
         else:
-            return 'Invalid username/password combination'
-            
-    return redirect(url_for('login'))
-        
-    
-    
+            return 'Invalid username/password'
+    else:
+        return redirect(url_for('register'))
+
+@app.route('/login_page')
+def login_page():
+     return render_template('login.html')
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
-
+        existing_user = users.find_one({'name': request.form['username']})
+          
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name':request.form['username'], 'password' : hashpass})
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        return 'Username already exists'
-    return render_template('register.html')
+            return redirect(url_for('get_games'))
+        else:
+            return 'Username already exists'
+    else:
+        return render_template('register.html')
 
 
 @app.route('/get_games')
@@ -111,7 +117,8 @@ def delete_game(game_id):
     return redirect(url_for('get_games'))
 
 if __name__ == '__main__':
+    
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
          port=os.environ.get('PORT'),
          debug=True)
-         
